91b0ae725eb28f3bb110bb81972feb840330a242
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-06-11 22:02:56 -0300

    Outra estrutura para representar os dados

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -17,6 +17,12 @@ public class Titulo implements Comparable <Titulo>{
         this.anoDeLancamento = anoDeLancamento;
     }
 
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 2));
+    }
+
     public void exibeFichaTecnica() {
             System.out.println("Nome do filme: " + nome);
             System.out.println("Ano de lançamento: " + anoDeLancamento);
@@ -77,7 +83,8 @@ public class Titulo implements Comparable <Titulo>{
 
     @Override
     public String toString() {
-        return "nome='" + nome + '\'' +
-                ", anoDeLancamento= " + anoDeLancamento;
+        return "nome = '" + nome + '\'' +
+                ", anoDeLancamento = " + anoDeLancamento + ","
+                + " duração = " + duracaoEmMinutos ;
     }
 }
