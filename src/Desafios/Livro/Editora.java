c13d6799e6d6d487ddd5eb812c88edcbccd896d3
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-06-11 23:27:25 -0300

    Desafio - 3

---
+++ src/Desafios/Livro/Editora.java
@@ -0,0 +1,4 @@
+package Desafios.Livro;
+
+public record Editora(String nome, String cidade) {
+}
---
+++ src/Desafios/Livro/Livro.java
@@ -0,0 +1,4 @@
+package Desafios.Livro;
+
+public record Livro(String titulo, String autor, Editora editora) {
+}
